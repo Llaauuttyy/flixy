50e92aa9d58e1af70f85361a5960892d8ac4cfc7
floritfran <fflorit@fi.uba.ar>
2025-06-08 13:53:47 -0300

    Agrego variables de entorno

--- backend/app/db/database_setup.py
+++ backend/app/db/database_setup.py
@@ -1,9 +1,10 @@
 from sqlmodel import create_engine
+from os import getenv
 
 USER = "root"
-PASSWORD = "root"
-HOST = "db"
-PORT = 3306
+PASSWORD = getenv("MYSQL_ROOT_PASSWORD")
+HOST = getenv("DB_HOST")
+PORT = int(getenv("INT_DB_PORT", 0))
 db_url = f"mysql+mysqlconnector://{USER}:{PASSWORD}@{HOST}:{PORT}/flixy"
 
 engine = create_engine(db_url)
