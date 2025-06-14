9dd1022ce533c6c4f601eac73e2b5c3b21cbaa58
Llaauuttyy <ldabbraccio@fi.uba.ar>
2025-06-13 23:23:57 -0300

    agrego validaciones al form de registro usando zod

--- frontend/app-react/components/register-form.tsx
+++ frontend/app-react/components/register-form.tsx
@@ -1,36 +1,41 @@
-import type React from "react"
-
-import { useActionState, useState } from "react"
+import { useForm } from "react-hook-form";
+import { useState } from "react"
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "../components/ui/button"
 import { Input } from "../components/ui/input"
 import { Label } from "../components/ui/label"
-import { Checkbox } from "../components/ui/checkbox"
-import { Loader2, Github, Mail } from "lucide-react"
-// import Link from "next/link"
-import { Link } from 'react-router-dom';
+import { Loader2 } from "lucide-react"
+import { SignupFormSchema } from "../lib/definitions";
 
-import { signup } from "../app/actions/auth";
+// Tipo derivado del esquema con zod
+type FormData = z.infer<typeof SignupFormSchema>;
 
-export default function RegisterForm() {
-  const [isLoading, setIsLoading] = useState(false)
-  const [name, setName] = useState("")
-  const [surname, setSurname] = useState("")
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [confirmed_password, setConfirmedPassword] = useState("")
+function RegisterForm() {
+  // Estados reactivos que re-renderizan el componente al actualizarse.
+  const [isLoading, setIsLoading] = useState(false);
+  const [pending, setPending] = useState(false);
 
-  const [state, action, pending] = useActionState(signup, undefined)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },  
+  } = useForm<FormData>({
+    resolver: zodResolver(SignupFormSchema),
+  });
 
-  // const handleSubmit = async (e: React.FormEvent) => {
-  //   e.preventDefault()
-  //   setIsLoading(true)
 
-  //   // Simulate API call
-  //   setTimeout(() => {
-  //     setIsLoading(false)
-  //     // Here you would normally redirect to dashboard or handle errors
-  //   }, 1500)
-  // }
+  const onSubmit = (data: FormData) => {
+    setIsLoading(true);
+    setPending(true);
+
+    console.log("Valid data:", data);
+
+    // TODO: Llamar API.
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1500)
+  };
 
   return (
     <div className="space-y-6">
@@ -48,116 +53,108 @@ export default function RegisterForm() {
           </div>
         </div>
 
-        <form action={action} className="space-y-4">
-            <div className="space-y-2">
-                <Label htmlFor="name" className="text-gray-300">
-                Name
-                </Label>
-                <Input
-                id="name"
-                name="name"
-                type="name"
-                placeholder="name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                required
-                className="bg-gray-800 border-gray-700 text-white placeholder:text-gray-500 focus-visible:ring-purple-500"
-                />
-            </div>
-            {state?.errors?.name && <p className="text-red-600">{state.errors.name}</p>}
-
-            <div className="space-y-2">
-                <Label htmlFor="surname" className="text-gray-300">
-                Surname
-                </Label>
-                <Input
-                id="surname"
-                name="surname"
-                type="surname"
-                placeholder="surname"
-                value={surname}
-                onChange={(e) => setSurname(e.target.value)}
-                required
-                className="bg-gray-800 border-gray-700 text-white placeholder:text-gray-500 focus-visible:ring-purple-500"
-                />
-            </div>
-            {state?.errors?.surname && <p className="text-red-600">{state.errors.surname}</p>}
-
-          <div className="space-y-2">
-            <Label htmlFor="email" className="text-gray-300">
-              Email
-            </Label>
-            <Input
-              id="email"
-              name="email"
-              type="email"
-              placeholder="name@example.com"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-              className="bg-gray-800 border-gray-700 text-white placeholder:text-gray-500 focus-visible:ring-purple-500"
-            />
-          </div>
-          {state?.errors?.email && <p className="text-red-600">{state.errors.email}</p>}
-
-          <div className="space-y-2">
-              <div className="flex items-center justify-between">
-              <Label htmlFor="password" className="text-gray-300">
-                  Password
-              </Label>
-              </div>
-              <Input
-              id="password"
-              name="password"
-              type="password"
-              placeholder="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-              className="bg-gray-800 border-gray-700 text-white placeholder:text-gray-500 focus-visible:ring-purple-500"
-              />
-          </div>
-          {state?.errors?.password && (
-            <div>
-              <p className="font-bold text-red-600">Password must:</p>
-              <ul>
-                {state.errors.password.map((error) => (
-                  <li className="text-red-600" key={error}>• {error}</li>
-                ))}
-              </ul>
-            </div>
-          )}
-
-          <div className="space-y-2">
-              <Input
-              id="password"
-              name="confirmPassword"
-              type="password"
-              placeholder="confirm password"
-              value={confirmed_password}
-              onChange={(e) => setConfirmedPassword(e.target.value)}
-              required
-              className="bg-gray-800 border-gray-700 text-white placeholder:text-gray-500 focus-visible:ring-purple-500"
-              />
-          </div>
-          {state?.errors?.confirmPassword && <p className="text-red-600">{state.errors.confirmPassword[0]}</p>}
-
-          <Button
-            disabled={pending}
-            type="submit"
-            className="w-full bg-gradient-to-r from-purple-500 to-pink-500 hover:from-purple-600 hover:to-pink-600 text-white"
-          >
-            {isLoading ? (
-              <>
-                <Loader2 className="mr-2 size-4 animate-spin" />
-                Registering...
-              </>
-            ) : (
-              "Sign up"
-            )}
-          </Button>
-        </form>
+    <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+      <div className="space-y-2">
+        <div>
+          <Label htmlFor="name" className="text-gray-300">
+            Name
+          </Label>
+        </div>
+        <Input {...register("name")}
+          id="name"
+          name="name"
+          type="name"
+          placeholder="name"
+          required
+          className="bg-gray-800 border-gray-700 text-white placeholder:text-gray-500 focus-visible:ring-purple-500"
+        />
+        {errors.name && <p style={{ color: "red" }}>{errors.name.message}</p>}
       </div>
+
+      <div className="space-y-2">
+        <div>
+          <Label htmlFor="surname" className="text-gray-300">
+            Surname
+          </Label>
+        </div>
+        <Input {...register("surname")}
+          id="surname"
+          name="surname"
+          type="surname"
+          placeholder="surname"
+          required
+          className="bg-gray-800 border-gray-700 text-white placeholder:text-gray-500 focus-visible:ring-purple-500"
+        />
+        {errors.surname && <p style={{ color: "red" }}>{errors.surname.message}</p>}
+      </div>
+
+      <div className="space-y-2">
+        <div>
+          <Label htmlFor="email" className="text-gray-300">
+            Email
+          </Label>
+        </div>
+        <Input {...register("email")}
+          id="email"
+          name="email"
+          type="email"
+          placeholder="name@example.com"
+          required
+          className="bg-gray-800 border-gray-700 text-white placeholder:text-gray-500 focus-visible:ring-purple-500"
+        />
+        {errors.email && <p style={{ color: "red" }}>{errors.email.message}</p>}
+      </div>
+
+      <div className="space-y-2">
+        <div className="flex items-center justify-between">
+          <div>
+            <Label htmlFor="password" className="text-gray-300">
+                Password
+            </Label>
+          </div>
+        </div>
+        <Input {...register("password")}
+          id="password"
+          name="password"
+          type="password"
+          placeholder="password"
+          required
+          className="bg-gray-800 border-gray-700 text-white placeholder:text-gray-500 focus-visible:ring-purple-500"
+        />
+        {errors.password && <p style={{ color: "red" }}>{errors.password.message}</p>}
+      </div>
+
+      <div className="space-y-2">
+        <Input {...register("confirmPassword")}
+          id="password"
+          name="confirmPassword"
+          type="password"
+          placeholder="confirm password"
+          required
+          className="bg-gray-800 border-gray-700 text-white placeholder:text-gray-500 focus-visible:ring-purple-500"
+          />
+          {errors.confirmPassword && <p style={{ color: "red" }}>{errors.confirmPassword.message}</p>}
+      </div>
+
+      <Button
+        disabled={pending}
+        type="submit"
+        className="w-full bg-gradient-to-r from-purple-500 to-pink-500 hover:from-purple-600 hover:to-pink-600 text-white"
+      >
+        {isLoading ? (
+          <>
+            <Loader2 className="mr-2 size-4 animate-spin" />
+              Registering...
+            </>
+        ) : (
+          "Sign up"
+        )}
+      </Button>
+    </form>
+
     </div>
-  )
+  </div>
+  );
 }
+
+export default RegisterForm;
