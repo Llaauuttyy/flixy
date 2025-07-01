3929fcbe9960ab29605995b06b9b78e03b992a22
floritfran <fflorit@fi.uba.ar>
2025-07-01 19:26:23 -0300

    Ajusto para poder acceder a las variables de entorno en el front

--- frontend/app-front/services/api/auth.ts
+++ frontend/app-front/services/api/auth.ts
@@ -1,40 +1,37 @@
-import type { LoginCredentials, RegistrationData } from './utils';
+import type { LoginCredentials, RegistrationData } from "./utils";
 
 export async function handleLogin(credentials: LoginCredentials) {
+  const response = await fetch(process.env.VITE_API_URL + "/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(credentials),
+  });
 
-    const response = await fetch(process.env.API_URL + "/login", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(credentials),
-    });
+  const response_json = await response.json();
 
-    const response_json = await response.json();
+  if (!response.ok) {
+    throw new Error(`${response_json.detail}`);
+  }
 
-    if (!response.ok) {
-        throw new Error(`${response_json.detail}`);
-    }
-
-    return response_json.access_token;
+  return response_json.access_token;
 }
 
 export async function handleRegistration(userData: RegistrationData) {
+  const response = await fetch(process.env.VITE_API_URL + "/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userData),
+  });
 
-    const response = await fetch(process.env.API_URL + "/register", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(userData),
-    });
+  const response_json = await response.json();
 
-    const response_json = await response.json();
+  if (!response.ok) {
+    throw new Error(`${response_json.detail}`);
+  }
 
-    if (!response.ok) {
-        throw new Error(`${response_json.detail}`);
-    }
-
-    return response_json.id;
+  return response_json.id;
 }
-  
--- frontend/app-front/vite.config.ts
+++ frontend/app-front/vite.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 export default defineConfig({
