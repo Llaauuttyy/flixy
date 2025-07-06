dbbbff88261bc927faad1178bb12c7e67ff80dd5
floritfran <fflorit@fi.uba.ar>
2025-07-06 20:07:41 -0300

    Cambio los mensaje de español a ingles

--- backend/app/constants/message.py
+++ backend/app/constants/message.py
@@ -1,9 +1,9 @@
 # User
-USER_NOT_FOUND = "Usuario no encontrado."
-EXISTENT_USERNAME_ERROR = "Ya existe otro usuario con ese nombre de usuario."
+USER_NOT_FOUND = "User not found."
+EXISTENT_USERNAME_ERROR = "There is already another user with that username."
 
 # Auth
-LOGIN_CREDENTIALS_ERROR = "Usuario o contraseña incorrectos."
-OLD_PASSWORD_DOESNT_MATCH_ERROR = "La contraseña no coincide con la contraseña anterior indicada."
-OLD_AND_NEW_PASSWORDS_ARE_THE_SAME_ERROR = "La contraseña anterior y la nueva son iguales."
-PASSWORD_VALIDATION_ERROR = "La contraseña debe contener: mayuscula, minuscula, numero y caracter especial."
+LOGIN_CREDENTIALS_ERROR = "Username or password are incorrect."
+OLD_PASSWORD_DOESNT_MATCH_ERROR = "Old password does not match current password."
+OLD_AND_NEW_PASSWORDS_ARE_THE_SAME_ERROR = "Old and new passwords are the same."
+PASSWORD_VALIDATION_ERROR = "Password must have: uppercase, lowercase, number and special character."
