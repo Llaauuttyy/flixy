0e0bcf6f59046f0432de3bdf9e046de8c73bc6d4
floritfran <fflorit@fi.uba.ar>
2025-07-18 19:28:22 -0300

    Agrego variables de entorno de la base de datos

--- backend/app/db/database_setup.py
+++ backend/app/db/database_setup.py
@@ -3,8 +3,8 @@ from os import getenv
 from typing import Annotated
 from fastapi import Depends
 
-USER = "root"
-PASSWORD = getenv("MYSQL_ROOT_PASSWORD")
+USER = getenv("DB_USER")
+PASSWORD = getenv("DB_PWD")
 HOST = getenv("DB_HOST")
 PORT = int(getenv("DB_PORT", 0))
 db_url = f"mysql+mysqlconnector://{USER}:{PASSWORD}@{HOST}:{PORT}/flixy"
