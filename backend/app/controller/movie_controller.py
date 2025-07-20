955484212d1b0c8f0ae8f60638b94556c39400eb
Llaauuttyy <ldabbraccio@fi.uba.ar>
2025-07-19 22:05:22 -0300

    agrego endpoint para obtener peliculas por id

--- backend/app/controller/movie_controller.py
+++ backend/app/controller/movie_controller.py
@@ -3,7 +3,7 @@ from app.db.database import Database
 from app.db.database_setup import SessionDep
 from app.service.movie_service import MovieService
 from app.dto.movie import MovieDTO
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Path, HTTPException
 from fastapi_pagination import Page, paginate
 
 movie_router = APIRouter()
@@ -14,3 +14,11 @@ MovieServiceDep = Annotated[MovieService, Depends(lambda: MovieService())]
 def get_movies(session: SessionDep, movie_service: MovieServiceDep) -> Page[MovieDTO]:
     movies = movie_service.get_all_movies(Database(session))
     return paginate(movies)
+
+@movie_router.get("/movie/{id}")
+def get_movie(session: SessionDep, movie_service: MovieServiceDep, id: int = Path(..., title="movie id", ge=1)) -> MovieDTO:
+    try:
+        movie = movie_service.get_movie_by_id(Database(session), id)
+        return movie
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=str(e))
--- backend/app/service/movie_service.py
+++ backend/app/service/movie_service.py
@@ -1,6 +1,7 @@
 from app.model.movie import Movie
 from app.db.database import Database
 from app.dto.movie import MovieDTO
+from app.constants.message import MOVIE_NOT_FOUND
 
 
 class MovieService:
@@ -22,3 +23,23 @@ class MovieService:
                 logo_url=movie.logo_url,
             ) for movie in movies
         ]
+    
+    def get_movie_by_id(self, db: Database, movie_id: int) -> MovieDTO:
+        movie = db.find_by(Movie, "id", movie_id)
+        if not movie:
+            raise Exception(MOVIE_NOT_FOUND)
+        
+        return MovieDTO(
+            id=movie.id,
+            title=movie.title,
+            year=movie.year,
+            imdb_rating=movie.imdb_rating,
+            genres=movie.genres,
+            countries=movie.countries,
+            duration=movie.duration,
+            cast=movie.cast,
+            directors=movie.directors,
+            writers=movie.writers,
+            plot=movie.plot,
+            logo_url=movie.logo_url,
+        )
