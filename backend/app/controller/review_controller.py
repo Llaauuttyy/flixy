c4c61f3a0a0b6a68fa6f8bfd26e5dd81bff7716b
llaauuttyy <ldabbraccio@fi.uba.ar>
2025-07-24 01:32:38 -0300

    agrego cambios sobre endpoint /revies/{movie_id} para manejar paginado y refactor

--- backend/app/controller/review_controller.py
+++ backend/app/controller/review_controller.py
@@ -2,9 +2,9 @@ from typing import Annotated
 from app.db.database import Database
 from app.db.database_setup import SessionDep
 from app.service.review_service import ReviewService
-from app.dto.review import ReviewDTO, ReviewCreationDTO, ReviewGetResponse, ReviewGetSingularDTO
+from app.dto.review import ReviewDTO, ReviewCreationDTO, ReviewGetSingularDTO, ReviewGetResponse
 from fastapi import APIRouter, Depends, Path, HTTPException, Request
-from fastapi_pagination import Page, paginate
+from fastapi_pagination import Page, paginate, Params
 from app.constants.message import REVIEWS_NOT_FOUND
 
 review_router = APIRouter()
@@ -12,22 +12,15 @@ review_router = APIRouter()
 ReviewServiceDep = Annotated[ReviewService, Depends(lambda: ReviewService())]
 
 @review_router.get("/review/{movie_id}")
-def get_reviews(session: SessionDep, request: Request, review_service: ReviewServiceDep, movie_id: int = Path(..., title="movie id", ge=1)) -> ReviewGetResponse:
+def get_reviews(session: SessionDep, request: Request, review_service: ReviewServiceDep, movie_id: int = Path(..., title="movie id", ge=1), params: Params = Depends()) -> ReviewGetResponse:
     user_id = request.state.user_id
 
     try:
-        current_user_review, reviews = review_service.get_all_reviews(Database(session), user_id, movie_id)
+        current_user_review, r_reviews = review_service.get_all_reviews(Database(session), user_id, movie_id)
 
         return ReviewGetResponse(
-            user_review=ReviewGetSingularDTO(
-                id=current_user_review.id,
-                user_id=current_user_review.user_id,
-                movie_id=current_user_review.movie_id,
-                text=current_user_review.text,
-                watch_date=current_user_review.watch_date,
-                user_name=current_user_review.user.name
-            ) if current_user_review else None,
-            reviews=paginate(reviews)
+            user_review=current_user_review,
+            reviews=paginate(r_reviews, params)
         )
     except HTTPException as http_exc:
         raise HTTPException(status_code=http_exc.status_code, detail=http_exc.detail)
--- backend/app/service/review_service.py
+++ backend/app/service/review_service.py
@@ -1,20 +1,42 @@
 from fastapi import HTTPException
 from app.model.review import Review
 from app.db.database import Database
-from app.dto.review import ReviewGetSingularDTO, ReviewGetResponse
+from app.dto.review import ReviewGetSingularDTO
 from sqlalchemy.exc import IntegrityError
-from app.constants.message import MOVIE_NOT_FOUND
+from app.constants.message import REVIEWS_NOT_FOUND
 from sqlalchemy.orm import selectinload
 from sqlalchemy import and_
 from typing import Tuple, List, Optional
 
 class ReviewService:
+    def set_up_review_singular_dto(self, review: Review) -> ReviewGetSingularDTO:
+        return ReviewGetSingularDTO(
+            id=review.id,
+            user_id=review.user_id,
+            movie_id=review.movie_id,
+            text=review.text,
+            watch_date=review.watch_date,
+            user_name=review.user.name
+        )
+
     def get_all_reviews(self, db: Database, user_id: int, movie_id: int) -> Tuple[Optional[ReviewGetSingularDTO], List[ReviewGetSingularDTO]]:
         current_user_review = db.find_by_multiple(Review, movie_id=movie_id, user_id=user_id)
         
         reviews = db.find_all(Review, and_(Review.movie_id == movie_id, Review.user_id != user_id), [selectinload(Review.user)])
 
         if not current_user_review and not reviews:
-            raise HTTPException(status_code=404, detail=MOVIE_NOT_FOUND)
+            raise HTTPException(status_code=404, detail=REVIEWS_NOT_FOUND)
+
+        if current_user_review:
+            current_user_review = self.set_up_review_singular_dto(current_user_review)
+
+        if reviews:
+            reviews_singular = list()
+            for review in reviews:
+                reviews_singular.append(
+                    self.set_up_review_singular_dto(review)
+                )
+
+            reviews = reviews_singular
 
         return (current_user_review, reviews)
