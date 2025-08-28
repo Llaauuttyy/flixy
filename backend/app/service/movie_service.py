cd8c77813b0a55137a555a32de26bc1eb70e1d98
floritfran <fflorit@fi.uba.ar>
2025-08-27 23:15:37 -0300

    Agrego parametro como filtro al endpoint de peliculas

--- backend/app/service/movie_service.py
+++ backend/app/service/movie_service.py
@@ -5,10 +5,13 @@ from app.dto.movie import MovieGetResponse
 from app.constants.message import MOVIE_NOT_FOUND
 
 class MovieService:
-    def get_all_movies(self, db: Database, user_id: int, order_column: str, order_way: str) -> list[MovieGetResponse]:
+    def get_all_movies(self, db: Database, user_id: int, order_column: str, order_way: str, genres: list[str] = None) -> list[MovieGetResponse]:
+        condition = [Movie.genres.ilike(f"%{genre}%") for genre in genres] if genres else []
+
         movies_rating = db.left_join(
             left_model=Movie,
             right_model=Review,
+            condition=db.build_condition(condition),
             join_condition=(db.build_condition([Movie.id == Review.movie_id, Review.user_id == user_id])),
             order_by={"way": order_way, "column": order_column}
         )
@@ -36,7 +39,7 @@ class MovieService:
             left_model=Movie,
             right_model=Review,
             join_condition=(db.build_condition([Movie.id == Review.movie_id, Review.user_id == user_id])),
-            and_filters=[Movie.id == movie_id]
+            condition=db.build_condition([Movie.id == movie_id])
         )
 
         if len(movies_rating) != 0:
@@ -64,12 +67,12 @@ class MovieService:
             left_model=Movie,
             right_model=Review,
             join_condition=(db.build_condition([Movie.id == Review.movie_id, Review.user_id == user_id])),
-            or_filters=[
+            condition=db.build_condition([
                 Movie.title.ilike(f"%{search_query}%"),
                 Movie.directors.ilike(f"%{search_query}%"),
                 Movie.cast.ilike(f"%{search_query}%"),
                 Movie.genres.ilike(f"%{search_query}%")
-            ]
+            ], "OR"),
         )
 
         return [
