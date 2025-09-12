1848abeb9a49bd84c9e9c5d9b5c4e40d676d54e7
Llaauuttyy <ldabbraccio@fi.uba.ar>
2025-09-12 00:54:01 -0300

    agrego modelos para las nuevas tablas

---
+++ backend/app/model/achievement.py
@@ -0,0 +1,18 @@
+from sqlmodel import Field, SQLModel, Relationship
+from datetime import datetime as datetime
+
+
+class Achievement(SQLModel, table=True):
+    __tablename__ = "achievements"
+
+    id: int = Field(default=None, primary_key=True)
+    name: str
+    description: str
+    icon_name: str
+    unlock_conditions: str
+    created_at: datetime = Field(default_factory=datetime.now)
+    updated_at: datetime = Field(default_factory=datetime.now)
+
+    user_achievements: list["UserAchievement"] = Relationship(back_populates="achievement")
+
+
